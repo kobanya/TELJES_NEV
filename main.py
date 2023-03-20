380a40121dd3292c92dc3289fb19c05a64ebc6b2
kobanya <nagy.bela.budapest@gmail.com>
2023-03-20 22:49:37 +0100

    Kiegészítve a feladat leírásával

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 '''
 Teljes név   NB  2023 03 20
+
+Készíts egy Python programot 1-teljes_nev.py néven, amely bekéri a Vezetéknevet és a Keresztnevet egy-egy változóba, majd összefűzi egy teljes_nev nevű változóba, amelyet ki kell íratni %s változójelölő segítségével!
+Segítség:
+(valtozo1 = input)
+(valtozo2 = input)
+(valtozo3 = valtozo1 + valtozo2)
+(kiíratás %s)
+
+Mintapélda
+Adja meg a vezetéknevét: Kovács
+Adja meg a keresztnevét: Dénes
+Üdv, Kovács Dénes!
+
 '''
 vezetek = input('Adja meg a vezeték nevét: ')  # egyik változó
 kereszt = input('Adja meg a keresztnévét: ')   # másik változó
