d69e0a27dbd82de21e0240a5615852c187d04958
kobanya <nagy.bela.budapest@gmail.com>
2023-03-20 22:36:22 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+'''
+Teljes név   NB  2023 03 20
+'''
+vezetek = input('Adja meg a vezeték nevét: ')  # egyik változó
+kereszt = input('Adja meg a keresztnévét: ')   # másik változó
+
+teljes_nev = vezetek + " " + kereszt  # szóköz a két változó között
+
+print("Üdv, %s!" % teljes_nev)   # string , kifejezés
